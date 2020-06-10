b6f499aabd214439ac0e0dcfbd6259b4886ddabc
Michael Karrer <michaelkarrer81@gmail.com>
2020-06-10 11:26:48 +0200

    [ADD] gl2k instance addons for merge test

---
+++ addons/gl2k_product_themes/models/product_template.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-'8' "-*-"
+from openerp import api, models, fields
+
+__author__ = 'Michael Karrer'
+
+
+# Product Template
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    website_theme = fields.Selection(selection_add=[('gl2k_inline', 'Global 2000 Inline'),
+                                                    ('gl2k_inline_test', 'Global 2000 Inline TEST')])
